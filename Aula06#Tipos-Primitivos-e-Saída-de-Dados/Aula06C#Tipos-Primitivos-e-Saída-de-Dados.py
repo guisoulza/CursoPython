e9e2a42c39a77683d3f30e81a138b081a7ea8d38
guisoulza <gui.souza9@hotmail.com>
2025-08-19 03:49:19 -0300

    Adicionado comentários.

--- Aula06#Tipos-Primitivos-e-Saída-de-Dados/Aula06C#Tipos-Primitivos-e-Saída-de-Dados.py
+++ Aula06#Tipos-Primitivos-e-Saída-de-Dados/Aula06C#Tipos-Primitivos-e-Saída-de-Dados.py
@@ -3,7 +3,7 @@
 #Formas de usar o tipo booleano.
 
 n=input('Digite algo: ')
-print(f'É {n.isnumeric()}')
-print(f'É {n.isalpha()}')
-print(f'É {n.isalnum()}')
+print(f'É {n.isnumeric()}')                 #Nesta linha o .isnumeric() vai mostrar se o dado é um número.
+print(f'É {n.isalpha()}')                   #Nesta linha o .isalpha() vai mostrar se o dado possui letras.
+print(f'É {n.isalnum()}')                   #Nesta linha o .isalnum() vai mostrar se o dado tem números ou letras.
 
