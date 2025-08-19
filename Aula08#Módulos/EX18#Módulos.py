919d6b9e445a8692e90b17eb0ab422166eb4f7c7
guisoulza <gui.souza9@hotmail.com>
2025-08-19 03:53:36 -0300

    Erros corrigidos

--- Aula08#Módulos/EX18#Módulos.py
+++ Aula08#Módulos/EX18#Módulos.py
@@ -1,13 +1,13 @@
 #Módulos.
 
-import math
-#from math import radians, sin, cos, tan
+#import math
+from math import radians, sin, cos, tan
 
 a=int(input('Digite o angulo: '))
-s=math.sin(math.radians(a))
-c=math.cos(math.radians(a))
-t=math.tan(math.radians(a))
-#s=sin(math.radians(a))
-#c=cos(math.radians(a))
-#t=tan(math.radians(a))
+#s=math.sin(math.radians(a))
+#c=math.cos(math.radians(a))
+#t=math.tan(math.radians(a))
+s=sin(radians(a))
+c=cos(radians(a))
+t=tan(radians(a))
 print('O valor do seno é {:.2f}, do cosseno é {:.2f} e da tangente é {:.2f}'.format(s, c, t))
