b2e4488b182b09c02507c31c39144db0ecc2cb9e
guisoulza <gui.souza9@hotmail.com>
2025-08-21 14:05:59 -0300

    Adicionado a resposta completa do exercício após estudos e a correção em seguida

--- Aula15#Interrompendo-Repetições-while/EX70#Interrompendo-Repetições-while.py
+++ Aula15#Interrompendo-Repetições-while/EX70#Interrompendo-Repetições-while.py
@@ -5,17 +5,51 @@
 #* Quantos produtos custam mais de R$1000;
 #* Qual produto mais barato.
 
-c = soma = 0
+cmil = soma = menor = c = 0
+barato = ''
 while True:
     produto = str(input('Digite o nome do produto: '))
-    valor = float(input('Digite o valor do produto: '))
+    valor = float(input('Digite o valor do produto: R$'))
+    c += 1
     soma += valor
     if valor >= 1000:
-        c += 1
+        cmil += 1
+    if c == 1:
+        menor = valor
+        barato = produto
+    else:
+        if valor < menor:
+            menor = valor
+            barato = produto
     opcao = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
     if opcao not in 'SN':
         print('Opção inválida')
         opcao = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
     if opcao == 'N':
         break
-print(f'O total das compras é R${soma:.2f}, com {c} produtos maiores que R$1000,00 e o produto mais barato é.')
+print(f'O total das compras é R${soma:.2f}, com {cmil} produtos maiores que R$1000,00 e o produto mais barato é {barato}.')
+
+#Correção
+
+"""
+total = totmil = menor = c = 0
+barato = ''
+while True:
+    produto = str(input('Digite o nome do produto: '))
+    preco = float(input('Digite o valor do produto: R$'))
+    cont += 1
+    total += preco
+    if preco >= 1000:
+        totmil += preco
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = produto
+    resp = ''
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('{:-^40}'.format('FIM DO PROGRAMA)
+print(f'O total da compra foi R${total:.2f}')
+print(f'O produto mais barato foi {barato} e custa R${menor:.2f})
+"""
