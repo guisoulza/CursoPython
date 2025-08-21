067f06c8eddd2b32a59babcb644973c720340164
guisoulza <gui.souza9@hotmail.com>
2025-08-21 14:05:09 -0300

    Adicionado uma correção na linha 7 e adicionado a correção

--- Aula15#Interrompendo-Repetições-while/EX66#Intenrrompendo-Repetições-while.py
+++ Aula15#Interrompendo-Repetições-while/EX66#Intenrrompendo-Repetições-while.py
@@ -4,9 +4,34 @@
 
 c = soma = 0
 while True:
-    n = int(input('Digite qualquer número: '))
+    n = int(input('Digite um valor (999 para parar): '))
     if n == 999:
         break
     c += 1
     soma += n
 print(f'foram contabilizados {c} números e a soma entre eles é {soma}')
+
+#Correção
+
+"""
+
+Forma incorreta:
+
+num = soma = 0
+while num != 999:
+    num = int(input('Digite um valor (999 para parar): '))
+    soma = += num
+soma -= 999
+print(f'A soma dos valores foi {soma}')
+
+Forma correta:
+
+soma = cont = 0
+while True:
+    num = int(input('Digite um valor (999 para parar): '))
+    if num == 999:
+        break
+    cont += 1
+    soma += num
+print(f'A soma dos {cont} valores foi {soma}')
+"""
