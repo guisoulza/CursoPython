80207c7c7f606bf78af737c80009cfcf9610d69c
guisoulza <gui.souza9@hotmail.com>
2025-08-23 09:44:42 -0300

    Correção adicionada

--- Aula15#Interrompendo-Repetições-while/EX71#Interrompendo-repetições-while.py
+++ Aula15#Interrompendo-Repetições-while/EX71#Interrompendo-repetições-while.py
@@ -3,7 +3,7 @@
 #71 - Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual valor será sacado (número inteiro) e o programa deve informar quantas cédulas de cada valor serão entregues. OBS: Caixa possui notas de 50, 20, 10 e 1.
 
 print('-' * 30)
-print('GELAS BANK')
+print(f'{'GELAS BANK':^30}')
 print('-' * 30)
 n = int(input('Digite o valor do saque: '))
 cedula = 50
@@ -25,10 +25,38 @@ while True:
         elif cedula == 10:
             nota10 = total_cedula
             cedula = 1
-            print(f'Serão necessárias {total_cedula} notas de 10.')
         elif cedula == 1:
             nota1 = total_cedula
             print(f'Serão necessárias {total_cedula} notas de 1.')
         total_cedula = 0
         if n == 0:
             break
+
+#Correção
+
+"""
+print('-' * 30)
+print(f'{'BANCO CEV':^30}')
+print('-' * 30)
+valor = int(input('Digite o valor do saque: '))
+total = valor
+ced = 50
+totalced = 0
+while True:
+    if valor >= ced:
+        total -= ced
+        tot ced += 1
+    else:
+    print(f'O total de {totced} cedulas de {ced}.')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
+"""
