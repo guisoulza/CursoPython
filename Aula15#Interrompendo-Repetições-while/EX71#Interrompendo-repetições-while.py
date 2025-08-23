8c904cbf7975c0c4315d6a4cca8dba2afa71ffde
guisoulza <gui.souza9@hotmail.com>
2025-08-23 09:12:38 -0300

    Adicionado a resposta completa do exercício após estudos

--- Aula15#Interrompendo-Repetições-while/EX71#Interrompendo-repetições-while.py
+++ Aula15#Interrompendo-Repetições-while/EX71#Interrompendo-repetições-while.py
@@ -2,6 +2,33 @@
 
 #71 - Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual valor será sacado (número inteiro) e o programa deve informar quantas cédulas de cada valor serão entregues. OBS: Caixa possui notas de 50, 20, 10 e 1.
 
+print('-' * 30)
+print('GELAS BANK')
+print('-' * 30)
 n = int(input('Digite o valor do saque: '))
-if n // 50 > 0:
-    print(f'Total de {n // 50} cédulas de 50.')
+cedula = 50
+total_cedula=0
+nota50 = nota20 = nota10 = nota1 = 0
+while True:
+    if n >= cedula:
+        n -= cedula
+        total_cedula += 1
+    else:
+        if cedula == 50:
+            nota50 = total_cedula
+            cedula = 20
+            print(f'Serão necessárias {total_cedula} notas de 50.')
+        elif cedula == 20:
+            nota20 = total_cedula
+            cedula = 10
+            print(f'Serão necessárias {total_cedula} notas de 20.')
+        elif cedula == 10:
+            nota10 = total_cedula
+            cedula = 1
+            print(f'Serão necessárias {total_cedula} notas de 10.')
+        elif cedula == 1:
+            nota1 = total_cedula
+            print(f'Serão necessárias {total_cedula} notas de 1.')
+        total_cedula = 0
+        if n == 0:
+            break
