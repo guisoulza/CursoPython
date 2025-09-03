aed52393a296022ab8fb3a8129728272e909b36f
guisoulza <gui.souza9@hotmail.com>
2025-09-03 17:08:39 -0300

    Adicionado a correção e um exercício bônus a pedido do professor

--- Aula16#Tuplas/EX72#Tuplas.py
+++ Aula16#Tuplas/EX72#Tuplas.py
@@ -4,4 +4,35 @@
 
 numeros = ("zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove", "vinte")
 usuario = int(input('Digite um número de 0 a 20: '))
-print(f'O numero digitado por extenso é {numeros[usuario]}')
+while True:
+    if usuario > 20:
+        print('Tente novamente!')
+        usuario = int(input('Digite um número de 0 a 20: '))
+    print( f'O numero digitado por extenso é {numeros[usuario]}')                  # Sempre lembrar que a tupla pode reconhecer dado do usuário se estiver nos [].
+    opcao = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    while opcao not in 'SN':
+        print('Tente novamente. ', end='')
+        opcao = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if opcao == 'S':
+        usuario = int(input('Digite um número de 0 a 20: '))
+    if opcao == 'N':
+        break
+
+#O professor passou um desafio bônus para adicionar a pergunta se o usuário deseja continuar
+
+# Correção
+
+"""
+cont = ("zero", "um", "dois", "três", "quatro", 
+    "cinco", "seis", "sete", "oito", "nove", 
+    "dez", "onze", "doze", "treze", "quatorze", 
+    "quinze", "dezesseis", "dezessete", "dezoito", 
+    "dezenove", "vinte")
+
+while True:
+    num = int(input('Digite um numero entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente novamente. ', end='')
+print(f'Você digitou o número {cont[num]}')
+"""
