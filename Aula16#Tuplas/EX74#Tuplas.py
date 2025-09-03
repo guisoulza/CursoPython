558fdd71f010b57ceabf4e08fd67cced22891ee4
guisoulza <gui.souza9@hotmail.com>
2025-09-03 17:30:07 -0300

    Adicionado a correção do exercício.

--- Aula16#Tuplas/EX74#Tuplas.py
+++ Aula16#Tuplas/EX74#Tuplas.py
@@ -5,8 +5,21 @@
 from random import randint
 
 tupla = (randint(1, 5), randint(1, 5), randint(1, 5), randint(1, 5), randint(1, 5))
-print(f'Tupla aleatória: {tupla}')
+print(f'Tupla aleatória: {tupla}')                              #Eu poderia usar o join() mas ele só funciona com strings
 print(f'O maior valor é: {max(tupla)}')
 print(f'O menor valor é: {min(tupla)}')
 
 #max() e min() facilitam demais a minha vida.
+
+#Correção
+
+"""
+from random import randint
+
+tupla = (randint(1, 5), randint(1, 5), randint(1, 5), randint(1, 5), randint(1, 5))
+print(f'Os números sorteados foram:', end=' ')
+for n in numeros:                                              #Não esquecer dessa forma de realizar a ordem de números sem aparecer os parentêses
+    print(f'{n}', end=' ')
+print(f'O maior valor é: {max(tupla)}')
+print(f'O menor valor é: {min(tupla)}')
+"""
