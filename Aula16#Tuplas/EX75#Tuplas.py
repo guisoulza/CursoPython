62a12b85463f4007dc14b7b7da8f36519201a178
guisoulza <gui.souza9@hotmail.com>
2025-09-06 12:24:15 -0300

    Adicionado a correção do exercício.

--- Aula16#Tuplas/EX75#Tuplas.py
+++ Aula16#Tuplas/EX75#Tuplas.py
@@ -20,3 +20,24 @@ for n in tupla:
     if n % 2 == 0:
         pares += (n,)
 print(f'Os pares foram: {pares}')
+
+#Correção
+
+"""
+num = (int(input('Digite um número: ')),
+       int(input('Digite outro número: ')),
+       int(input('Digite mais um número: ')),
+       int(input('Digite o últmo número: ')))
+print(f'voce digitou os valores {num}')
+print(f'O valor 9 apareceu {num.count(9)} vezes.')
+if 3 in num:
+    print(f'O valor 3 apareceu na {num.index(3)+1}ª posição')
+else:
+    print('O valor 3 não aparece na tupla')
+print('Os valores pares digitados foram: ', end='')
+for n in num:
+    if n % 2 == 0:
+        print(n, end=' '))
+"""
+
+
