3acd93644593c033503c7272e6fcbd7c46771549
guisoulza <gui.souza9@hotmail.com>
2025-09-17 09:49:51 -0300

    Adicionado a correção do exercício

--- Aula17#Listas/EX79#Listas.py
+++ Aula17#Listas/EX79#Listas.py
@@ -1,6 +1,6 @@
 # Listas.
 
-# 79 - Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em um lista.
+# 79 - Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista.
 # Caso o número já exista lá dentro, ele não será adicionado.
 # No final, serão exibidos todos os valores únicos digitados, em ordem crescente
 
@@ -19,3 +19,25 @@ while True:
     elif fim == 'N':
         break
 print(f'Os valores digitados foram {sorted(lista)}')
+
+# Correção
+
+"""
+numeros = list()
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado na lista! Não será adicionado')
+    r = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if r not in 'SN':
+        print('Opção invalida! Tente novamente.')
+        r = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    elif r in 'N':
+        break
+print('-=' * 30)
+numeros.sort()
+print(f'Voce digitou os valores: {numeros}')
+"""
