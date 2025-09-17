9937fe611266fb15b71ca2004cae15bd22184247
guisoulza <gui.souza9@hotmail.com>
2025-09-17 10:07:14 -0300

    Adicionado a correção do exercício

--- Aula17#Listas/EX81#Listas.py
+++ Aula17#Listas/EX81#Listas.py
@@ -19,6 +19,28 @@ while True:
 print(f'Foram digitados {len(lista)} elementos.')
 print(f'O inverso é: {sorted(lista, reverse=True)}')
 if 5 in lista:
+    print('O valor 5 faz parte da lista.')
+else:
+    print('O valor 5 não foi encontrado na lista.')
+
+# Correção
+
+"""
+valores = []
+while True:
+    valores.append(int(input('Digite um valor: ')))
+    resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp not in 'SN':
+        print('Opção invalida!')
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    elif resp == 'N':
+        break
+print('-=' * 30)
+print(f'Você digitou {len(valores)} elementos.')
+valores.sort(reverse=True)
+print(f'Os valores em ordem decrescente: {valores}')
+if 5 in valores:
     print('O valor 5 está na lista.')
 else:
     print('O valor 5 não está na lista.')
+"""
