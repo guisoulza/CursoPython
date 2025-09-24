4d957e874fbfc6d8b0e44c66bdce45999ad89ee6
guisoulza <gui.souza9@hotmail.com>
2025-09-24 16:12:48 -0300

    Adicionado a correção do exercício.

--- Aula17#Listas(Parte 2)/EX89#Listas(Parte 2).py
+++ Aula17#Listas(Parte 2)/EX89#Listas(Parte 2).py
@@ -30,3 +30,29 @@ while True:
         print(f'Notas de {boletim[selecao][0]} são {boletim[selecao][1]} {boletim[selecao][2]}')
 print('-=' * 30)
 
+# Correção
+
+"""
+ficha = list()
+while True:
+    nome = str(input('Digite o nome do aluno: ')).title().strip()
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('-=' * 30)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-=' * 35)
+    opc = int(input('Mostrar nota de qual aluno? (999 para parar): '))
+    if opc == 999:
+        print('FINALIZANDO...')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]} {ficha[opc][2]}')
+"""
