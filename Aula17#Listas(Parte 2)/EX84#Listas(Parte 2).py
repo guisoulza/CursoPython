26db0cb9e88989aeeb20238bf8df7eaa189409b7
guisoulza <gui.souza9@hotmail.com>
2025-09-24 15:22:50 -0300

    Adicionado a correção do exercício.

--- Aula17#Listas(Parte 2)/EX84#Listas(Parte 2).py
+++ Aula17#Listas(Parte 2)/EX84#Listas(Parte 2).py
@@ -32,3 +32,38 @@ for p in pessoas:
 
 print(f'{pesados} estão com mais de 100 kg.')
 print(f'{leves} estão com menos de 70 kg.')
+
+# Correção
+
+"""
+temp = []
+princ = []
+mai = men = 0
+
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    princ.append(temp[:])
+    if len(temp) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+    temp.clear()
+    resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('-=' * 30)
+print(f'Ao todo. você cadastrou {len(pessoas)} pessoas. ')
+print(f'O maior peso foi de {mai}Kg. Peso de ', end='')
+for p in princ:
+    if p[1] == mai:
+        print(f'[{p[0]}] ', end='')
+print()
+print(f'O menor peso foi de {men}Kg. Peso de ', end='')
+for p in princ:
+    if p[1] == men:
+        print(f'[{p[0]}] ', end='')
+"""
