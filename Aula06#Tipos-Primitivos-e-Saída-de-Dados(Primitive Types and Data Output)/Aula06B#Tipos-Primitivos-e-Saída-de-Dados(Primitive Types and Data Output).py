463e525b77e314a04bdbe4eb6d55480dc0b85a73
guisoulza <gui.souza9@hotmail.com>
2025-10-07 17:12:04 -0300

    Add English version of primitive types section

--- Aula06#Tipos-Primitivos-e-Saída-de-Dados(Primitive Types and Data Output)/Aula06B#Tipos-Primitivos-e-Saída-de-Dados(Primitive Types and Data Output).py
+++ Aula06#Tipos-Primitivos-e-Saída-de-Dados(Primitive Types and Data Output)/Aula06B#Tipos-Primitivos-e-Saída-de-Dados(Primitive Types and Data Output).py
@@ -5,7 +5,7 @@ nf=float(input('Digite um valor: '))                      # Dado do tipo float p
 print(type(nf))                                                 # Data type float for decimal or floating-point numbers.
 print(nf)
 ns=str(input('Digite um valor: '))                                                # Dado do tipo string para caracteres.
-print(type(ns))                                                                   # Data type string for characters
+print(type(ns))                                                                       # Data type string for characters.
 print(ns)
 nb=bool(input('Digite um valor: '))              # Dado do tipo bool para valores verdadeiros ou falsos (True ou False).
 print(type(nb))                                               # Data type bool for true ou false values (True or false).
