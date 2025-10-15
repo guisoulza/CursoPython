9d6919afb3929b638d188092dc361557172a0df6
guisoulza <gui.souza9@hotmail.com>
2025-10-15 17:29:19 -0300

    Added lesson 19 with examples and practice

---
+++ Aula19#Dicionários/Aula19A#Dicionários.py
@@ -0,0 +1,12 @@
+pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 22}
+print(pessoas)
+print(pessoas['nome'])
+print(pessoas['idade'])
+print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos.')
+print(pessoas.keys())
+print(pessoas.values())
+print(pessoas.items())
+pessoas['nome'] = 'Leandro'
+pessoas['peso'] = 98.5
+for k, v in pessoas.items():
+    print(f'{k} = {v}')
---
+++ Aula19#Dicionários/README.md
@@ -0,0 +1,7 @@
+# Aula 19
+
+# Dicionários
+
+Nessa aula, vamos aprender o que são DICIONÁRIOS e como utilizar dicionários em Python. Os dicionários são variáveis 
+compostas que permitem armazenar vários valores em uma mesma estrutura, acessíveis por chaves literais.
+
