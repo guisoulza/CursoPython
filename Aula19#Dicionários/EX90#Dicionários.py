842ed9990f7beafe6d3b0dce3c1ef0db22e72a64
guisoulza <gui.souza9@hotmail.com>
2025-10-21 16:51:27 -0300

    Added the answer of exercises 90 and 91

--- Aula19#Dicionários/EX90#Dicionários.py
+++ Aula19#Dicionários/EX90#Dicionários.py
@@ -2,3 +2,14 @@
 
 # 90 - Faça um programa que leia nome e média de um aluno, guardando tambem a situação em um dicionário. No final mostre
 # o conteúdo da estrutura na tela.
+
+boletim = {}
+boletim['nome'] = str(input('Nome: '))
+boletim['média'] = int(input(f'Média do {boletim["nome"]}: '))
+print('-=' * 30)
+print(f' - O nome é igual a {boletim['nome']}')
+print(f' - Média é igual a {boletim['média']}')
+if boletim['média'] < 7:
+    print(' - Situação é Recuperação')
+else:
+    print(' - situação é Aprovado')
--- Aula19#Dicionários/EX91#Dicionários.py
+++ Aula19#Dicionários/EX91#Dicionários.py
@@ -2,3 +2,21 @@
 
 # 91 - Crie um programa onde 4 jogadores joguem um dado e tenha resultados aleatórios. Guarde esses resultados em um
 # dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+
+from random import randint
+from time import sleep
+
+dado = {}
+p = 0
+for c in range(0, 4):
+    dado[f'jogador {c+1}'] = randint(1, 6)
+    print(f'jogador {c+1} tirou {dado[f'jogador {c+1}']} no dado.')
+    sleep(1)
+for i in range(len(dado)):
+    maior = max(dado.values())
+    for k in dado:
+        if dado[k] == maior:
+            p += 1
+            print(f'{p}º Lugar: {k} com {maior}')
+            dado[k] = -1
+            break
