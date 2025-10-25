a5939aa77cfe7c613e6bfd17fe37b78afe0acb68
guisoulza <gui.souza9@hotmail.com>
2025-10-25 16:15:57 -0300

    Added the answer of exercise 92.

--- Aula19#Dicionários/EX92#Dicionários.py
+++ Aula19#Dicionários/EX92#Dicionários.py
@@ -3,3 +3,20 @@
 # 92 - Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um
 # dicionário se por acaso a CTPS for diferente de 0, o dicionário receberá tambem o ano de contratação e o salário.
 # Calcule e acrescente, alem da idade, com quantos anos a pessoa vai se aposentar.
+
+from datetime import date
+
+dados = dict()
+dados['nome'] = str(input('Nome: '))
+dados['Nascimento'] = int(input('Ano de nascimento: '))
+dados['idade'] = date.today().year - dados['Nascimento']
+dados['CTPS'] = int(input('CTPS (0 não tem): '))
+if dados['CTPS'] != 0:
+    dados['Contratação'] = int(input('Ano de contratação: '))
+    dados['Salário'] = int(input('Salário: R$'))
+    dados['Aposentadoria'] = (dados['Contratação'] - dados['Nascimento']) + 35
+
+print('-=' * 30)
+for k, v in dados.items():
+    if k != 'Nascimento':
+        print(f' - {k} tem o valor {v}')
