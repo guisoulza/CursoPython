73100b1efb42aab90b9d48ddd9b51860d360bc6a
guisoulza <gui.souza9@hotmail.com>
2025-10-27 16:39:18 -0300

    Added the answer of exercise 94.

--- Aula19#Dicionários/EX94#Dicionários.py
+++ Aula19#Dicionários/EX94#Dicionários.py
@@ -8,3 +8,40 @@
 # * Uma lista com todas as mulheres.
 # * Uma lista com todas as pessoas com idade acima da média.
 
+dado = []
+mulheres = []
+idademed = []
+c= medidade = total = 0
+while True:
+    dados = {}
+    dados['Nome'] = str(input('Nome: '))
+    dados['Sexo'] = str(input('Sexo: '))
+    if dados['Sexo'] not in 'FfMm':
+        print('Genero invalido tente novamente.')
+        dados['Sexo'] = str(input('Sexo: '))
+    dados['Idade'] = int(input('Idade: '))
+    dado.append(dados.copy())
+    c += 1
+    cont = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if cont == 'N':
+        break
+print('-=' * 30)
+print(f'Foram cadastradas {c} pessoas.')
+for p in dado:
+    total += p['Idade']
+    medidade = total / c
+print(f'A média de idade é {medidade:.2f} anos.')
+for p in dado:
+    if p['Sexo'] == 'F':
+        mulheres.append(p['Nome'])
+print(f'Lista de mulheres cadastradas: {mulheres}')
+for p in dado:
+    if p['Idade'] >= medidade:
+        idademed.append(p['Nome'])
+print(f'Pessoas acima da média: {idademed}')
+print('-=' * 30)
+
+
+
+
+
